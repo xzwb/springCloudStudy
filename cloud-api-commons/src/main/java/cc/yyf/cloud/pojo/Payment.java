6687e05874086c4a43fa6a7f91040794df3080c7
xzwb <1478863479@qq.com>
2020-08-08 15:13:25 +0800

    refactor
    将pojo类提取出来

--- cloud-api-commons/src/main/java/cc/yyf/cloud/pojo/Payment.java
+++ cloud-api-commons/src/main/java/cc/yyf/cloud/pojo/Payment.java
@@ -3,14 +3,12 @@ package cc.yyf.cloud.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.ibatis.type.Alias;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Alias("payment")
 public class Payment implements Serializable {
     private int id;
     private String serial;
