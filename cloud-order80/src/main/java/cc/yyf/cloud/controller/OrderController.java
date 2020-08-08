7bd901f4fbe2a3504dc840fcddd9c31c3afdc046
xzwb <1478863479@qq.com>
2020-08-08 14:44:17 +0800

    test
    订单客户端模块

---
+++ cloud-order80/src/main/java/cc/yyf/cloud/controller/OrderController.java
@@ -0,0 +1,27 @@
+package cc.yyf.cloud.controller;
+
+import cc.yyf.cloud.pojo.Payment;
+import cc.yyf.cloud.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderController {
+    public static final String URL = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/payment/create")
+    public Result<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(URL+"/payment/create", payment, Result.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public Result<Payment> getPayment(@PathVariable("id") int id) {
+        return restTemplate.getForObject(URL+"/payment/get/"+id, Result.class);
+    }
+}
